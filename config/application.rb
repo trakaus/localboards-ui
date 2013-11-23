6d00c5817a3bbd2c26e4ae453692de819c59d57a
Mitchell Barry <mitch.barry@gmail.com>
2013-11-23 13:03:11 -0600

    try to initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,6 @@ module LocalboardsUi
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
   end
 end
